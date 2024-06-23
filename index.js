6da27d357d5f8a062df40f354af04cc4d6a40c55
Devloper-solaiman <devlopersolaiman@gmail.com>
2024-06-23 12:19:48 +0600

    vercel url problem 3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000
 const corsConfig = {
     origin: '*',
     credential: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
+    methods: ["GET", "POST", "DELETE"]
 }
 app.options("", cors(corsConfig))
 app.use(cors(corsConfig))
