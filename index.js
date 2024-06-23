46c26322a33d7306d5de3d9f7ab8ebeb2cfea437
Devloper-solaiman <devlopersolaiman@gmail.com>
2024-06-23 10:07:44 +0600

    Blog section post get delete

--- index.js
+++ index.js
@@ -32,9 +32,10 @@ const run = async() => {
         const collection = db.collection("data")
         const projectCollection = db.collection('project');
         const skillCollection = db.collection('skill');
+        const blogCollection = db.collection('blog');
 
 
-
+        // project selection
         app.post("/api/v1/project", async(req, res) => {
             const { _id, image, title, liveLink, clientLink, serverLink, technology, keyFeature, ratting, description } = req.body;
             const result = await projectCollection.insertOne({ _id, image, title, liveLink, clientLink, serverLink, technology, keyFeature, ratting, description });
@@ -66,7 +67,7 @@ const run = async() => {
         });
 
 
-        // skill worker
+        // skill section
 
         app.post("/api/v1/skill", async(req, res) => {
             const { _id, image, skillName, percentage } = req.body;
@@ -94,10 +95,45 @@ const run = async() => {
             });
             res.json({
                 success: true,
-                message: "successfully delete project!",
+                message: "successfully delete skill!",
                 data,
             });
         });
+
+
+        // Blog section
+
+        app.post("/api/v1/blog", async(req, res) => {
+            const { _id, image, title, readingTime, author, technology, shortDescription, description, publishDate, createdAt, } = req.body;
+            const result = await blogCollection.insertOne({ _id, image, title, readingTime, author, technology, shortDescription, description, publishDate, createdAt, });
+            res.json({
+                success: true,
+                message: "Successfully Blog create!",
+                result,
+            });
+        });
+
+        app.get("/api/v1/blog", async(req, res) => {
+            const data = await blogCollection.find({}).toArray();
+            res.json({
+                success: true,
+                message: "successfully retrieve Blog!",
+                data,
+            });
+        });
+
+        app.delete("/api/v1/blog/:id", async(req, res) => {
+            const { id } = req.params;
+            const data = await blogCollection.deleteOne({
+                _id: new ObjectId(id),
+            });
+            res.json({
+                success: true,
+                message: "successfully delete Blog!",
+                data,
+            });
+        });
+
 
     } finally {}
 }
